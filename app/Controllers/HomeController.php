69104d64dd4114f4e37309074356bdb0ba1567e1
Koltenssh-add <minixk1@gmail.com>
2026-01-29 17:33:53 -0500

    Controller Updates

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,11 +6,17 @@ namespace App\Controllers;
 
 use App\Support\Request;
 use App\Support\Response;
-
+use App\Support\View;
 final class HomeController
 {
     public function index(Request $request, array $params = []): Response
     {
-        return new Response("Home Great", 200);
+
+        $html = View::render('home.index', [
+            'title' => 'Home',
+            'heading' => 'Home',
+            'message' => 'Views are wired up'
+        ]);
+        return  Response::html($html);
     }
 }
