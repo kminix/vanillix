97fcdf7b9b5a5233f7e90cc43e7f88fcc074f6e6
Koltenssh-add <minixk1@gmail.com>
2026-06-26 10:56:57 -0400

    Base

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1); ?>
 <div class="card">
-  <h1><?= \App\Support\View::e($heading ?? 'Home') ?></h1>
+  <h1><?= \App\Support\View::e($heading ?? 'Base') ?></h1>
   <p><?= \App\Support\View::e($message ?? 'Welcome.') ?></p>
 </div>
